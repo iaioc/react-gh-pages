0eccf544bacb98129e8fee4e40e58b6d6ec617bf
Pooja Tewari <tewaripooja10@gmail.com>
2020-09-10 01:28:43 -0400

    Updates

---
+++ precache-manifest.ee6971e25ada2f2d091138a607c241ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98f4eb5fcd977910902d8d8ef9e5398",
+    "url": "/react-gh-pages/index.html/index.html"
+  },
+  {
+    "revision": "e6493aaabc22fe6db63c",
+    "url": "/react-gh-pages/index.html/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-gh-pages/index.html/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/index.html/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6493aaabc22fe6db63c",
+    "url": "/react-gh-pages/index.html/static/js/main.2ae4dc72.chunk.js"
+  },
+  {
+    "revision": "41b918aa53103bdbd5b3",
+    "url": "/react-gh-pages/index.html/static/js/runtime-main.83be5fc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
