01a4b92dac0b70cfec7a996d746b7c54e0f11914
Pooja Tewari <tewaripooja10@gmail.com>
2020-09-10 12:22:13 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/iaioclogo.png';
 import './App.css';
-
+import './css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src={logo} width="auto" height="30" alt="" loading="lazy"></img>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+             {/*} <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>*/}
+            </ul>
+          </div>
+        </nav>
+        <a class="navbar-brand" href="#">
+            <img src={logo} width="auto" height="500" alt="" loading="lazy"></img>
+          </a>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>HELLO WORLD</p>
     </div>
   );
 }
